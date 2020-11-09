048eaf64f97246fda4b19fd343c5c20701761900
Abramov3IP <72501957+Abramov3IP@users.noreply.github.com>
2020-11-09 15:46:28 +0300

    Add files via upload

---
+++ ses1/ses1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ses1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string login = "Pavel";
+        public string password = "1234";
+        public string user_login;
+        public string user_password;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            user_login = TextLogin.Text;
+            user_password = TextPass.Password;
+            if(user_login == login && user_password == password)
+            {
+                MessageBox.Show(";)");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
